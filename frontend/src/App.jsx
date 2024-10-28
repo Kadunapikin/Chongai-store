e8ed111d9c66fdd48fb10adaabdaf87c8c6680d5
Kadunapikin <ibakoshi27@gmail.com>
2024-10-28 12:28:14 +0100

    add tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <h1 className="text-3xl bg-slate-500 font-bold underline">
+      Hello world!
+    </h1>  )
 }
 
 export default App
