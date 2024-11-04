c34faa408bef113362b45de3c838008bd58e6707
Kadunapikin <ibakoshi27@gmail.com>
2024-11-04 23:33:00 +0100

    add variables to ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { createContext } from "react";
+import { products } from "../assets/assets";
 
-const ShopContext = () => {
-  return (
-    <div>ShopContext</div>
-  )
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+
+    const currency = '$';
+    const delivery_fee = 10;
+
+    const value = {
+        products , currency , delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
 }
 
-export default ShopContext
+export default ShopContextProvider;
