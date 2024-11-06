6dd8a9f293fd4f53a56ebcb8beb019fc5278422c
Kadunapikin <ibakoshi27@gmail.com>
2024-11-06 23:33:08 +0100

    import ShopContext to LatestCollection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+
   return (
-    <div>LatestCollection</div>
+    <div></div>
   )
 }
 
