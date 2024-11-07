65b3a5875b05601382d5140de612d89607d41c7e
Kadunapikin <ibakoshi27@gmail.com>
2024-11-07 11:21:58 +0100

    add useState and useEffect to LatestCollection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 
 const LatestCollection = () => {
 
-    const { products } = useContext(ShopContext);    
+    const { products } = useContext(ShopContext); 
+    const [latestProduct, setLatestProduct] = useState([]);   
+
+    useEffect(() => {
+        setLatestProduct(products.slice(0,10));
+    }, [])
 
   return (
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores asperiores, non cumque distinctio.
+            </p>
         </div>
     </div>
   )
