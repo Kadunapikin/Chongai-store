25bdf31a38f9876d15d50dda03618e1f3ba83f8e
Kadunapikin <ibakoshi27@gmail.com>
2024-11-12 00:50:38 +0100

    update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -4,14 +4,13 @@ import Title from './Title';
 import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
-
-    const { products } = useContext(ShopContext); 
-    const [latestProducts, setLatestProducts] = useState([]);   
+    const { products } = useContext(ShopContext)
+    const [latestProducts, setLatestProducts] = useState([]);
 
     useEffect(() => {
-        setLatestProducts(products.slice(0, 10));
-    }, [])
-
+        setLatestProducts(products.slice(0,10));
+    }, []);
+    
   return (
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
@@ -22,7 +21,7 @@ const LatestCollection = () => {
         </div>
         
         {/* Rendering Products */}
-        <div className='grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
             {
                 latestProducts.map((item, index) => (
                     <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
