367b83451473e5d614493bbedf71665b78719092
Kadunapikin <ibakoshi27@gmail.com>
2024-11-11 23:23:58 +0100

    update ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,8 +8,10 @@ const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
 
+
     const value = {
         products , currency , delivery_fee
+
     }
 
     return (
@@ -17,6 +19,30 @@ const ShopContextProvider = (props) => {
             {props.children}
         </ShopContext.Provider>
     )
+
 }
 
 export default ShopContextProvider;
+
+// import { createContext } from "react";
+// import { products } from "../assets/assets";
+
+// export const ShopContext = createContext();
+
+// const ShopContextProvider = (props) => {
+
+//     const currency = '$';
+//     const delivery_fee = 10;
+
+//     const value = {
+//         products , currency , delivery_fee
+//     }
+
+//     return (
+//         <ShopContext.Provider value={value}>
+//             {props.children}
+//         </ShopContext.Provider>
+//     )
+// }
+
+// export default ShopContextProvider;
