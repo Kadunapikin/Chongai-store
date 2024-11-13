f7d48a9b15ae78bbdb755a3fc3856706d7240427
Kadunapikin <ibakoshi27@gmail.com>
2024-11-13 17:01:45 +0100

    display bestProducts on the Home page

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,17 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const BestSeller = () => {
 
     const {products} = useContext(ShopContext);
     const [bestSeller, setBestSeller] = useState([]);
+    
 
     useEffect(() => {
-        const bestProduct = products.filter((item) => (item.BestSeller));
+        const bestProduct = products.filter((item) => (item.bestseller));
+        
         setBestSeller(bestProduct.slice(0, 5));
-
     }, []);
+    
 
   return (
     <div className='my-10'>
@@ -20,6 +23,13 @@ const BestSeller = () => {
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores asperiores, non cumque distinctio.
             </p>
+        </div>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                bestSeller.map((item, index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                ))
+            }
         </div>
 
     </div>
