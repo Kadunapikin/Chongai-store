c6c6cf7022a5fa80f54f981ef3c4910e42b0709a
Kadunapikin <ibakoshi27@gmail.com>
2024-11-14 16:43:47 +0100

    create Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=''>
+        {/* <p>Copyright © 2022 Your Company. All rights reserved.</p>
+        <ul>
+          <li><a href='#'>Privacy Policy</a></li>
+          <li><a href='#'>Terms & Conditions</a></li>
+        </ul>
+        <p>Powered by <a href='https://www.yourcompany.com'>Your Company</a></p>
+        <div className='social-media'>
+          <a href='#'><i className='fab fa-facebook-f'></i></a>
+          <a href='#'><i className='fab fa-twitter'></i></a>
+          <a href='#'><i className='fab fa-instagram'></i></a>
+          <a href='#'><i className='fab fa-pinterest-p'></i></a>
+        </div> */}
+    </div>
+  )
+}
+
+export default Footer
