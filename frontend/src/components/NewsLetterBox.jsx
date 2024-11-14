f0ea82cc623324f87b6ee1d473cbf892b94ed3de
Kadunapikin <ibakoshi27@gmail.com>
2024-11-14 16:21:23 +0100

    create NewsLetterBox component

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subscribe Now and get 20% off</p>
+    </div>
+  )
+}
+
+export default NewsLetterBox
