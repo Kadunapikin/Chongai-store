9e4778e8ba9d6ba17cf63992b1db9edc3720cd94
Kadunapikin <ibakoshi27@gmail.com>
2024-11-14 18:25:18 +0100

    add items to Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className=''>
-        {/* <p>Copyright © 2022 Your Company. All rights reserved.</p>
-        <ul>
-          <li><a href='#'>Privacy Policy</a></li>
-          <li><a href='#'>Terms & Conditions</a></li>
-        </ul>
-        <p>Powered by <a href='https://www.yourcompany.com'>Your Company</a></p>
-        <div className='social-media'>
-          <a href='#'><i className='fab fa-facebook-f'></i></a>
-          <a href='#'><i className='fab fa-twitter'></i></a>
-          <a href='#'><i className='fab fa-instagram'></i></a>
-          <a href='#'><i className='fab fa-pinterest-p'></i></a>
-        </div> */}
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <p className='w-full md:w-2/3 text-gray-600'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aliquid illum nisi aspernatur possimus ut vero  ex quos iusto.
+                </p>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/about'>About Us</a></li>
+                    <li><a href='#'>Delievery</a></li>
+                    <li><a href='#'>Careers</a></li>
+                    <li><a href='#'>Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+234-7036-071-563</li>
+                    <li>ibakoshi27@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ bakoshi.com - All Right Reserved.</p>
+        </div>
     </div>
   )
 }
