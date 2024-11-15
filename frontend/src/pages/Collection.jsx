76608f9729fbbc3172157c3671ca9324973757a5
Kadunapikin <ibakoshi27@gmail.com>
2024-11-15 16:01:41 +0100

    add showCategory function

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,16 +1,23 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
 
 const Collection = () => {
 
     const { products } = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
 
+    const showCategory = () => {
+        setShowFilter(!showFilter);
+    }
+
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
         {/* Filter options */}
         <div className='min-w-60'>
-            <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+            <p onClick={showCategory} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+                <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
+            </p>
             {/* category filter */}
             <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
                 <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
@@ -27,7 +34,7 @@ const Collection = () => {
                 </div>
             </div>
             {/* Sub category filter */}
-            <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+            <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
                 <p className='mb-3 text-sm font-medium'>TYPE</p>
                 <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
                     <p className='flex gap-2'>
