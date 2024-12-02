c8a8a435641163602e452e446816fc728ede3976
Kadunapikin <ibakoshi27@gmail.com>
2024-12-02 18:21:51 +0100

    save productData in different state

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
   const {productId} = useParams();
-  console.log(productId);
+  const {products} = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
+
+  const fetchProductData = async () => {
+    products.map((product) => {
+      if (product._id === productId) {
+        setProductData(product);
+        setImage(product.image[0]);
+        return null;
+      }
+    })
+  }
   
+  useEffect(() => {
+    fetchProductData();
+  }, [productId]);
 
 
-  return (
-    <div>Product</div>
-  )
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        
+      </div>
+    </div>
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
