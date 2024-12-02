c8c038b3364c09184c4d430f5cd41bd0bade1100
Kadunapikin <ibakoshi27@gmail.com>
2024-12-02 18:22:54 +0100

    display product images in Product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -28,6 +28,25 @@ const Product = () => {
       {/* Product data */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
         
+        {/* Product images */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img
+                  key={index}
+                  src={item}
+                  alt={productData.title}
+                  className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'
+                  onClick={() => setImage(item)}
+                />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img src={image} alt="" className='w-full h-auto'/>
+          </div>
+        </div>
       </div>
     </div>
   ) : <div className='opacity-0'></div>
