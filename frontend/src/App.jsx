8f99bf0b2254d8898d3b76ee769ee0a746b01240
Kadunapikin <ibakoshi27@gmail.com>
2024-12-10 15:38:27 +0100

    import ToastContainer to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,13 @@ import Product from './pages/Product'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <NavBar />
       <SearchBar />
       <Routes>
