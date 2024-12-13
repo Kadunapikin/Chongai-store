dce6d31d50be417129e4d40296f72757e83ddd5f
Kadunapikin <ibakoshi27@gmail.com>
2024-12-13 16:30:16 +0100

    import product details from ShopContext and write a logic to display selected items

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
+
+  const { products , currency , cardItems } = useContext(ShopContext); 
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+
+    const tempData = [];
+
+    for(const items in cardItems) {
+      for(const item in cardItems[items]) {
+        if (cardItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cardItems[items][item],            
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+    
+  }, [cardItems]);
+
   return (
     <div>Cart</div>
   )
