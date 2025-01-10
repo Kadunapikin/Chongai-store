17fb45f59d291425fddf1e2c5c5dcd26e9d1e5cb
Kadunapikin <ibakoshi27@gmail.com>
2025-01-10 23:51:41 +0100

    Add payment methods

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
 
 const PlaceOrder = () => {
   return (
@@ -30,6 +31,24 @@ const PlaceOrder = () => {
       <div className='mt-8'>
         <div className='mt-8 min-w-80'>
           <CartTotal />
+        </div>
+        <div className='mt-12'>
+          <Title text1={'PAYMENT'} text2={'METHOD'} />
+          {/* -------------------------Payment method selection------------------------ */}
+          <div className='flex gap-3 flex-col lg:flex-row'>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
+            </div>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
+            </div>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+            </div>
+          </div>
         </div>
       </div>
 
