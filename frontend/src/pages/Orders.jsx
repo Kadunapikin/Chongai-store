a4bb2554a247126f3b4f3eaf86ea784b7ed033f7
Kadunapikin <ibakoshi27@gmail.com>
2025-01-21 12:24:23 +0100

    add item details on orders page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -16,6 +16,15 @@ const Orders = () => {
           <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
             <div className='flex items-start gap-6 text-sm'>
               <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
+              <div>
+                <p className='sm:text-base font-medium'>{item.name}</p>
+                <div className='flex items-center gap-3 mt-2 text-base text-gray-700 '>
+                  <p className='text-lg'>{currency}{item.price}</p>
+                  <p>Quantity: 1</p>
+                  <p>Size: M</p>
+                </div>
+                <p className='mt-2'>Date: <span className='text-gray-400'>20th, Jan, 2025</span></p>
+              </div>
             </div>
           </div>
         ))
