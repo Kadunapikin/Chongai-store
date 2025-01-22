bfe654f18293d4bc9dccc1eed729e4b8dbf81d35
Kadunapikin <ibakoshi27@gmail.com>
2025-01-22 13:27:13 +0100

    add currentState state on login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [currentState, setCurrentState] = useState('Sign Up');
+
   return (
-    <div>Login</div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 text-gray-800 gap-4'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+    </form>
   )
 }
 
