a56bbb23712431f84fec3edbc744e7962ebf4373
Kadunapikin <ibakoshi27@gmail.com>
2025-01-23 17:56:54 +0100

    add a logic to prevent page reloading onsubmit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,8 +4,12 @@ const Login = () => {
 
   const [currentState, setCurrentState] = useState('Sign Up');
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 text-gray-800 gap-4'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 text-gray-800 gap-4'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
         <p className='prata-regular text-3xl'>{currentState}</p>
         <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
@@ -21,6 +25,7 @@ const Login = () => {
           : <p onClick={() => setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
         }
       </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
     </form>
   )
 }
