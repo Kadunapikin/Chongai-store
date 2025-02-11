9b882da5d592db8fe693df42e091a3d8c086500b
Kadunapikin <ibakoshi27@gmail.com>
2025-02-11 16:38:05 +0100

    create mongodb.js file

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     mongoose.connection.on('connected', () => {
-        console.log('MongoDB connected');
+        console.log('MongoDB connected succesfully');
     })
     await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`)
 }
