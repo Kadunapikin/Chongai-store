17b65ad460f2855a8936e4b5ba39282ef7977734
Kadunapikin <ibakoshi27@gmail.com>
2025-03-06 14:56:00 +0100

    create productController.js

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+//function to add product
+const addProduct = async (req, res) => {
+
+};
+
+//function to list product
+const listProduct = async (req, res) => {
+
+};
+
+//function for removing product
+const removeProduct = async (req, res) => {
+
+};
+
+//function for single product infor
+const singleProduct = async (req, res) => {
+
+}
+
+export { addProduct, listProduct, removeProduct, singleProduct };
