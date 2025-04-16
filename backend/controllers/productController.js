3a83c3d4ef8bb12703476e4716ea93389311836d
Kadunapikin <ibakoshi27@gmail.com>
2025-04-16 16:46:38 +0100

    create route to list all products in database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -45,30 +45,15 @@ const addProduct = async (req, res) => {
     }
 };
  
-//New add product function
-// const addProduct = async (req, res) => {
-//     try {
-//         const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
-
-//         const image1 = req.files?.image1?.[0];
-//         const image2 = req.files?.image2?.[0];
-//         const image3 = req.files?.image3?.[0];
-//         const image4 = req.files?.image4?.[0];
-
-//         console.log(name, description, price, category, subCategory, sizes, bestSeller);
-//         console.log(image1, image2, image3, image4);
-
-//         res.json({}); // No validation, just respond
-
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: error.message });
-//     }
-// };
-
 //function to list product
 const listProduct = async (req, res) => {
-
+    try {
+        const products = await productModel.find({});
+        res.json({success: true, products})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 };
 
 //function for removing product
