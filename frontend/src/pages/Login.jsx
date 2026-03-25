c38c12922b735e424546c67f56caf299cce01179
Kadunapikin <ibakoshi27@gmail.com>
2026-03-25 15:02:26 +0100

    add login logic to frondend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
@@ -17,14 +18,27 @@ const Login = () => {
     try {
       if (currentState === 'Sign Up') {
         const response = await axios.post(backendUrl + '/api/user/register', {name, email, password});
-
-        console.log(response.data);
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem('token', response.data.token);
+          // navigate('/');
+        } else {
+          toast.error(response.data.msg);
+        }
         
       } else {
-        
+        const response = await axios.post(backendUrl + '/api/user/login', {email, password});
+        if (response.data.success) {
+          setToken(response.data.token);          
+          localStorage.setItem('token', response.data.token);
+          // navigate('/');
+        } else {
+          toast.error(response.data.msg);
+        }
       }
     } catch (error) {
-      
+      console.log(error);
+      toast.error(error.message);
     }
   }
 
